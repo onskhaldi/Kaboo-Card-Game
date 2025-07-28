aed507b0ebb3fea9755e6246d1cc1adce3f46bf4
onskhaldi <onskhaldi367@gmail.com>
2025-07-28 22:33:46 +0100

    entity tests  dokumentation

--- src/test/kotlin/entity/cardTest.kt
+++ src/test/kotlin/entity/cardTest.kt
@@ -1,9 +1,17 @@
 package entity
 
 import kotlin.test.*
-
+/**
+ * Tests für die [Card]-Klasse.
+ */
 class CardTest {
-
+    /**
+     * Testet die Initialisierung einer Karte sowie deren [toString]-Darstellung.
+     *
+     * Überprüft:
+     * - Farbe (suit), Wert (value) und Powercard-Flag
+     * - korrekte String-Darstellung, z. B. "♥7"
+     */
     @Test
     fun testCardInitializationAndToString() {
         val card = Card(CardSuit.HEARTS, CardValue.SEVEN, true)
@@ -14,6 +22,12 @@ class CardTest {
         assertEquals("♥7", card.toString())
     }
 
+    /**
+     * Testet Gleichheit von Karten.
+     *
+     * Überprüft:
+     * - equals() erkennt identische Karten korrekt
+     */
     @Test
     fun testCardEquality() {
         val card1 = Card(CardSuit.SPADES, CardValue.KING, false)
@@ -23,6 +37,13 @@ class CardTest {
         assertEquals(card1.hashCode(), card2.hashCode())
     }
 
+    /**
+     * Testet die `copy()`-Funktion der Karte.
+     *
+     * Überprüft:
+     * - Inhaltliche Gleichheit der Kopie
+     * - Unterschiedliche Referenzen (nicht dasselbe Objekt)
+     */
     @Test
     fun testCardCopy() {
         val original = Card(CardSuit.CLUBS, CardValue.TEN, true)
@@ -31,9 +52,11 @@ class CardTest {
         assertEquals(original, copy)
         assertNotSame(original, copy)
     }
-
+    /**
+     * Testet das Verändern des `isPowercard nach der Initialisierung.
+     */
     @Test
-    fun testMutablePowerCardFlag() {
+    fun testPowerCard() {
         val card = Card(CardSuit.DIAMONDS, CardValue.ACE, false)
         card.isPowercard = true
         assertTrue(card.isPowercard)
