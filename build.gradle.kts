03222da9a97f9f83e4f9143f09ac37e9e682c058
onskhaldi <onskhaldi367@gmail.com>
2025-08-31 16:37:51 +0100

    Clean Gradle setup: remove SoPra plugin, make project standalone

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,11 @@
-import edu.udo.cs.sopra.util.ignoreClassesInCoverageReport
-import edu.udo.cs.sopra.util.sonatypeSnapshots
-import org.gradle.kotlin.dsl.application
-
 plugins {
     kotlin("jvm") version "1.9.25"
-    application
-    id("edu.udo.cs.sopra") version "1.0.3"
+    id("application")
 }
 
-group = "edu.udo.cs.sopra"
+group = "com.onskhaldi.kaboo"
 version = "1.0"
 
-/* Change this to the version of the BGW you want to use */
 val bgwVersion = "0.10"
 
 kotlin {
@@ -24,25 +18,17 @@ application {
 
 repositories {
     mavenCentral()
-    sonatypeSnapshots()
 }
 
 dependencies {
+    testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
-    implementation(group = "tools.aqua", name = "bgw-gui", version = bgwVersion)
-    implementation(group = "tools.aqua", name = "bgw-net-common", version = bgwVersion)
-    implementation(group = "tools.aqua", name = "bgw-net-client", version = bgwVersion)
+
+    implementation("tools.aqua:bgw-gui:$bgwVersion")
+    implementation("tools.aqua:bgw-net-common:$bgwVersion")
+    implementation("tools.aqua:bgw-net-client:$bgwVersion")
 }
 
-/* This is how you can ignore additional classes from test coverage */
-/* All classes in gui, entity and service.bot package are already excluded. */
-
-/* To ignore a class Foo in the package foo.bar.baz you would use the following line */
-// this.ignoreClassesInCoverageReport("foo.bar.baz.Foo")
-
-/* To ignore all classes in the foo.bar.baz package use a wildcard like this */
-// this.ignoreClassesInCoverageReport("foo.bar.baz.*")
-
 tasks.clean {
-    delete.add("public")
+    delete("public")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,7 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven {
-            url = uri(providers.gradleProperty("sopra-gitlab.package-registry.url").get())
-            credentials(HttpHeaderCredentials::class.java) {
-                name = "Private-Token"
-                value = providers.gradleProperty("sopra-gitlab.package-registry.token").get()
-            }
-            authentication {
-                create("header", HttpHeaderAuthentication::class.java)
-            }
-        }
+        mavenCentral()
     }
 }
 
@@ -18,4 +9,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "projekt1"
+rootProject.name = "Kaboo-Card-Game"
